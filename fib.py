c5e58d8cbe60415da93d17b61ac92a86c57f94ef
Ricardo Lafuente <r@manufacturaindependente.org>
2014-11-27 23:13:01 +0000

    Fontpkg generation, convert refactor

--- fib.py
+++ fib.py
@@ -1,7 +1,11 @@
 import sys
 import os
+import shutil
+import codecs
+import json
 import click
 import subprocess
+from zenlog import log
 try:
     import fontforge
 except:
@@ -22,6 +26,43 @@ def cli():
     pass
 
 
+def _convert_fontfile(fontfile, format):
+    font = fontforge.open(fontfile)
+    d = os.path.dirname(os.path.abspath(fontfile)) + '/'
+    filename = os.path.basename(fontfile)
+    basename, ext = os.path.splitext(filename)
+    if format == "woff":
+        filename = d + basename + '.woff'
+        font.generate(filename)
+    elif format == "otf":
+        filename = d + basename + '.otf'
+        font.generate(filename)
+    elif format == "ttf":
+        filename = d + basename + '.ttf'
+        font.generate(filename)
+    elif format == "svg":
+        filename = d + basename + '.svg'
+        font.generate(filename)
+    elif format == "ufo":
+        filename = d + basename + '.ufo'
+        font.generate(filename)
+    elif format == "sfd":
+        filename = d + basename + '.sfd'
+        font.save(filename)
+    elif format == "eot":
+        filename = d + basename + '.eot'
+        if ext == ".ttf":
+            cmd = 'ttf2eot %s %s' % (fontfile, filename)
+            os.system(cmd)
+        else:
+            ttf_filename = d + basename + '.ttf'
+            font.generate(ttf_filename)
+            cmd = 'ttf2eot %s %s' % (ttf_filename, filename)
+            os.system(cmd)
+            os.remove(ttf_filename)
+    return filename
+
+
 @cli.command()
 @click.option("-w", "--woff", is_flag=True, help="WOFF format")
 @click.option("-t", "--ttf", is_flag=True, help="TrueType format")
@@ -37,41 +78,20 @@ def convert(fontfiles, woff, ttf, otf, svg, sfd, ufo, eot, pack_webfont):
     """Convert fonts to and from various formats"""
     for fontfile in fontfiles:
         fontfile = click.format_filename(fontfile)
-        font = fontforge.open(fontfile)
-
-        d = os.path.dirname(os.path.abspath(fontfile)) + '/'
-        filename = os.path.basename(fontfile)
-        basename, ext = os.path.splitext(filename)
-
         if woff:
-            woff_filename = d + basename + '.woff'
-            font.generate(woff_filename)
+            _convert_fontfile(fontfile, "woff")
         if otf:
-            otf_filename = d + basename + '.otf'
-            font.generate(otf_filename)
+            _convert_fontfile(fontfile, "otf")
         if ttf:
-            ttf_filename = d + basename + '.ttf'
-            font.generate(ttf_filename)
+            _convert_fontfile(fontfile, "ttf")
         if svg:
-            svg_filename = d + basename + '.svg'
-            font.generate(svg_filename)
+            _convert_fontfile(fontfile, "svg")
         if ufo:
-            ufo_filename = d + basename + '.ufo'
-            font.generate(ufo_filename)
+            _convert_fontfile(fontfile, "ufo")
         if sfd:
-            sfd_filename = d + basename + '.sfd'
-            font.save(sfd_filename)
+            _convert_fontfile(fontfile, "sfd")
         if eot:
-            eot_filename = d + basename + '.eot'
-            if ttf:
-                cmd = 'ttf2eot %s %s' % (ttf_filename, eot_filename)
-                os.system(cmd)
-            else:
-                ttf_filename = d + basename + '.ttf'
-                font.generate(ttf_filename)
-                cmd = 'ttf2eot %s %s' % (ttf_filename, eot_filename)
-                os.system(cmd)
-                os.remove(ttf_filename)
+            _convert_fontfile(fontfile, "eot")
 
 
 @cli.command()
@@ -86,26 +106,73 @@ def pkg():
     pass
 
 
-@click.option("-y", "--yes", is_flag=True, help="Assume yes answer to all prompts (non-interactive)")
-@click.argument("fontfiles", nargs=-1, type=click.Path(exists=True))
+def _create_fontpkg(fontfile, yes=False):
+    pass
+
+
+@click.option("-y", "--yes", is_flag=True, help="Assume yes answer to all prompts (non-interactive)", default=False)
+@click.argument("fontfile", type=click.Path(exists=True))
 @pkg.command()
-def create():
+def create(fontfile, yes):
     """Takes an existing font file and generates a font package."""
     # get fontfiles
+    font = fontforge.open(fontfile)
     # prompt if ok to create pkg dir
-    # convert to UFO
+    pkg_dir = font.familyname + ".fontpkg"
+    if os.path.exists(pkg_dir):
+        if not yes:
+            click.confirm("Font package %s already exists. Delete and regenerate?" % pkg_dir, abort=True)
+        shutil.rmtree(pkg_dir)
+        os.mkdir(pkg_dir)
+    # convert to UFO and reopen
+    ufo_dir = _convert_fontfile(fontfile, "ufo")
+    target = os.path.join(os.path.basename(pkg_dir), os.path.basename(ufo_dir))
+    shutil.move(ufo_dir, target)
     # generate JSON
+    font_info = {
+        "fullname": font.fullname,
+        "fontname": font.fontname,
+        "weight": font.weight,
+        "familyname": font.familyname,
+        "copyright": font.copyright,
+        "version": font.version,
+        "fontlog": font.fontlog,
+    }
+    json_file = os.path.join(pkg_dir, "fontpackage.json")
+    f = codecs.open(json_file, 'w', 'utf-8')
+    f.write(json.dumps(font_info, indent=4))
+    f.close()
+    # generate FONTLOG
+    if font.fontlog:
+        fontlog_file = os.path.join(pkg_dir, "FONTLOG")
+        f = codecs.open(fontlog_file, 'w', 'utf-8')
+        f.write(font.fontlog)
+        f.close()
     # generate README
+    readme_file = os.path.join(pkg_dir, "README")
+    f = codecs.open(readme_file, 'w', 'utf-8')
+    if font.comment:
+        f.write(font.comment)
+    else:
+        f.write("This is the README file. Markdown is supported. Start by mentioning your authorship and other details about this family.\n")
+    f.close()
+    log.info("%s created." % pkg_dir)
 
 
 @pkg.command()
 def sync():
     """Updates the font files in a package to match fontpackage.json metadata."""
+    # read datapackage JSON
+    # read font file
+    # compare values and update them
 
 
 @pkg.command()
 def validate():
     """Checks if the font package is valid."""
+    # check if JSON passes
+    # check attributes
+    # check if font files are readable and conform
 
 
 @click.option("-a", "--angle", help="Angle in degrees", default=45)
@@ -164,8 +231,12 @@ def foundry():
     """Commands for generating a HTML showcase."""
 
 
+@click.option("-c", "--config-file", help="Config file location (default: current dir)", type=click.Path(exists=True), default="./foundrysettings.conf")
+@click.argument("fontfiles", nargs=-1, type=click.Path(exists=True))
 @foundry.command()
 def generate():
+    # read foundrysettings.conf
+    # generate HTML site
     pass
 
 
